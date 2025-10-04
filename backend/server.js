54b40d3a7c75f86fd9d003969ee5141ea3a75449
Parjad Minooei <minooeip@gmail.com>
2025-10-04 18:49:14 -0400

    major upgrade

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,18 @@ import contentRoutes from './routes/contentRoutes.js';
 import amazonRoutes from './routes/amazonRoutes.js';
 import clickBankRoutes from './routes/clickBankRoutes.js';
 import bayPartnerRoutes from './routes/bayPartnerRoutes.js';
+// 1. Import the new user routes
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(cors());
 
@@ -26,6 +32,8 @@ app.use('/api/content', contentRoutes);
 app.use('/api/amazon', amazonRoutes);
 app.use('/api/clickbank', clickBankRoutes);
 app.use('/api/baypartner', bayPartnerRoutes);
+app.use('/api/users', userRoutes);
+
 
 const PORT = process.env.PORT || 5001;
 
